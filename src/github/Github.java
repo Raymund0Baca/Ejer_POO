0a6569b2b69f2ff88c442479ac0cdfee92fbf8ed
Raymundo <Raymundo@DESKTOP-BAC2TE1>
2021-04-19 12:58:10 -0600

    Ejercicio_poo

---
+++ src/github/Github.java
@@ -0,0 +1,20 @@
+/*
+Ejercicios para visualizar en Github
+ */
+package github;
+
+/**
+ *
+ * @author Raymundo
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola grupo de POO");
+        
+    }
+    
+}
